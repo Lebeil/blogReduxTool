5b4b14c9cf0865e32b985703a2c4582312710e0e
Liedel <liedel75@gmail.com>
2023-08-24 23:22:41 +0200

    final project

---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+import PropTypes from "prop-types";
+
+const PostAuthor = ({ userId }) => {
+  const users = useSelector(selectAllUsers);
+
+  const author = users.find((user) => user.id === userId);
+
+  return <span>by {author ? author.name : "Unknown author"}</span>;
+};
+
+PostAuthor.propTypes = {
+  userId: PropTypes.number,
+};
+
+export default PostAuthor;
